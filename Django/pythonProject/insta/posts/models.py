6a5e0e8ad890adad437f666d351917f4eab798ef
jay <jayfreemandev@gmail.com>
2023-10-20 16:59:06 +0900

    인스타그램

---
+++ Django/pythonProject/insta/posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        verbose_name="작성자",
+        on_delete=models.CASCADE
+    )
+    content = models.TextField("내용")
+    created = models.DateTimeField("생성일시", auto_now_add=True)
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, verbose_name="포스트", on_delete=models.CASCADE)
+    photo = models.ImageField("사진", upload_to="post")
+
+class Comment(models.Model):
+    user = models.ForeignKey("users.User", verbose_name="작성자", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name="포스트", on_delete=models.CASCADE)
+    content = models.TextField("내용")
+    created = models.DateTimeField("작성일시", auto_now_add=True)
---
+++ Django/pythonProject/insta/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from posts.views import feeds
+
+urlpatterns = [
+    path("feeds/", feeds),
+]
