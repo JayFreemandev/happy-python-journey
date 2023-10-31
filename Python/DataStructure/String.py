c55d67cd0dff4ced32908f8255fbd5e7a217a5d2
jaeyun <jayfreemandev@gmail.com>
2023-10-31 10:37:07 +0900

    string to char

--- Python/DataStructure/String.py
+++ Python/DataStructure/String.py
@@ -1,3 +1,19 @@
 a = [1,2,3]
 a.append(4)
 print(a)
+
+
+
+x = 'banana'
+print(x[2:4])
+
+
+# string to char
+characters = []
+sentence = 'be happy'
+for char in sentence:
+    characters.append(char)
+print(characters)
+
+# list to char
+print(list("be happy"))
