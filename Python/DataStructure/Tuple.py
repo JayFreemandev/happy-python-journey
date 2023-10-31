d75c80f8c879bf9dd6073b0fc39ada349893bef3
jaeyun <jayfreemandev@gmail.com>
2023-10-31 10:46:14 +0900

    tuple test

--- Python/DataStructure/Tuple.py
+++ Python/DataStructure/Tuple.py
@@ -1,6 +1,25 @@
-t1 = ()
-t2 = (1,)
-t3 = (1,2,3)
-t4 = ('a', 'b', ('c','d'))
+# t1 = ()
+# t2 = (1,)
+# t3 = (1,2,3)
+# t4 = ('a', 'b', ('c','d'))
 
-del t1[0]
+# del t1[0]
+
+
+# before tuple
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print(a,b)
+
+# after tuple
+c = 10
+d = 20
+c, d = d, c
+print(c, d)
+
+
+def magu_print(x, y, *rest):
+    print(x, y, rest)
